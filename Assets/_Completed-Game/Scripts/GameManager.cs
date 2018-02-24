4f4dac5e92ebe7e11a713de5b0223e99d864cb83
Lucy Chen <lucychen@Lucys-MBP.wv.cc.cmu.edu>
2018-02-24 18:22:18 -0500

    initialization

---
+++ Assets/_Completed-Game/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class GameManager : MonoBehaviour {
+
+	private int x = -10;
+
+	private void Start () {
+		BeginGame();
+	}
+
+	private void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			RestartGame();
+		}
+	} 
+
+	private void BeginGame () {
+		for (int z = -10; z < 10; z++) {
+			
+			GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			cube.AddComponent<Rigidbody>();
+			cube.transform.position = new Vector3(x, 1, z);
+			x = x + 1;
+		}
+	}
+
+	private void RestartGame () {}
+}
