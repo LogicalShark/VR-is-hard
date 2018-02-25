eead4bd4694eb98e319a3c308c7a7631897a3718
Lucy Chen <lucychen@Lucys-MBP.wv.cc.cmu.edu>
2018-02-25 00:00:08 -0500

    wider

--- Assets/_Completed-Game/Scripts/GameManager.cs
+++ Assets/_Completed-Game/Scripts/GameManager.cs
@@ -5,24 +5,28 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 
 	public GameObject house;
-	public GameObject cat;
-	public GameObject block;
-	public GameObject guard;
 	public GameObject ground;
+	public GameObject cat;
+	public GameObject roadblock;
+	public GameObject guard;
+	public GameObject screw;
+	public GameObject battery;
+	public GameObject gear;
 	public int width;
 	public int gameLength;
 	public int sectionLength;
 	public int houseLength;
 	public int floorLength;
 	private GameObject[] randBlock = new GameObject[3];
+	private GameObject[] randItem = new GameObject[3];
 
 	private void Start () {
-		GameObject cat1 = cat;
-		GameObject block1 = block;
-		GameObject guard1 = guard;
-		randBlock [0] = cat1;
-		randBlock [1] = block1;
-		randBlock [2] = guard1;
+		randBlock [0] = cat;
+		randBlock [1] = guard;
+		randBlock [2] = gear;
+		randItem [0] = screw;
+		randItem [1] = battery;
+		randItem [2] = roadblock;
 		BeginGame();
 	}
 
@@ -33,6 +37,8 @@ public class GameManager : MonoBehaviour {
 	} 
 
 	private void BeginGame () {
+
+		//Creating the houses
 		for (int z = -10; z < gameLength; z+=houseLength) 
 		{
 			GameObject houseRight = Instantiate(house);
@@ -40,19 +46,35 @@ public class GameManager : MonoBehaviour {
 			GameObject houseLeft = Instantiate(house);
 			houseLeft.transform.position = new Vector3(-width, -1, z);
 		}
-		for (int z = -10; z < gameLength-sectionLength; z += sectionLength) {
-			Random rnd = new Random();
-			int nextBlockPosZ = Random.Range (0, sectionLength-1);
-			int nextBlockPosX = Random.Range (-width, width);
-			int nextBlock = Random.Range(0, randBlock.Length);
-			GameObject block = Instantiate(randBlock [nextBlock]);
-			block.transform.position = new Vector3(nextBlockPosX, 2, z+nextBlockPosZ);
-		}
+
+		//Creating the floor
 		for (int z = -10; z < gameLength-floorLength; z += floorLength) 
 		{
 			GameObject floor = Instantiate(ground);
 			floor.transform.position = new Vector3(0, 0, z);
 		}
+
+		//Creating the roadblocks
+		for (int z = -10; z < gameLength-sectionLength; z += sectionLength) {
+			Random rnd = new Random();
+			int nextBlockPosZ = Random.Range (0, sectionLength-1);
+			int nextBlockPosX = Random.Range (-width+1, width-1);
+			int nextBlock = Random.Range(0, randBlock.Length);
+			GameObject newBlock = Instantiate(randBlock [nextBlock]);
+			newBlock.transform.position = new Vector3(nextBlockPosX, 2, z+nextBlockPosZ);
+		}
+
+		//Creating the items
+		for (int z = -10; z < gameLength-sectionLength; z += sectionLength) {
+			Random rnd = new Random();
+			int nextItemPosZ = Random.Range (0, sectionLength-1);
+			int nextItemPosX = Random.Range (-width+1, width-1);
+			int nextItem = Random.Range(0, randItem.Length);
+			GameObject newItem = Instantiate(randItem [nextItem]);
+			newItem.transform.position = new Vector3(nextItemPosX, 2, z+nextItemPosZ);
+		}
+
+			
 
 	}
 
