076b80c4ddf651739a482362879dc1cbba392a65
LogicalShark <malder@andrew.cmu.edu>
2018-02-24 19:45:14 -0500

    main menu

---
+++ Assets/ButtonScript1.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class Control : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Roll-a-ball");
+    }
+}
